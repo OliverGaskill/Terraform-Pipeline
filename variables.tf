c35df4a3633af47fea100c64e50099f5da2ec180
OliverGaskill <gaskilloliver@gmail.com>
2025-09-08 09:41:39 +0200

    basic terraform structure

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-example"
+}
