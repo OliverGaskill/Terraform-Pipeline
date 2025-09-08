30b361883a49a9212fd0501021b00ee36c54b937
OliverGaskill <gaskilloliver@gmail.com>
2025-09-08 10:30:36 +0200

    basic terraform structure

--- azureExample/modules/naming/main.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  rg_name   = "${var.company}-${var.project}-${var.environment}-rg"
-  vnet_name = "${var.company}-${var.project}-${var.environment}-vnet"
-
-  # Storage account must be lowercase, only alphanumeric, <= 24 chars
-  sa_name   = substr(lower(replace("${var.company}${var.project}${var.environment}st", "/[^a-z0-9]/", "")), 0, 24)
-}
