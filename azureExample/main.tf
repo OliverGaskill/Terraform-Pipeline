512a458b9a40995166aceac70d884a5447c29872
OliverGaskill <gaskilloliver@gmail.com>
2025-09-26 12:01:49 +0200

    fixed tf code

--- azureExample/main.tf
+++ azureExample/main.tf
@@ -6,8 +6,8 @@ resource "azurerm_resource_group" "rg" {
 
 # Storage Account
 resource "azurerm_storage_account" "storage-acc" {
-  name                     = lower("${var.company}-${var.project}-${var.environment}sa")
-  resource_group_name      = azurerm_resource_group.main.name
+  name                     = "storrage-acc"
+  resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -16,29 +16,32 @@ resource "azurerm_storage_account" "storage-acc" {
 # Virtual Network
 resource "azurerm_virtual_network" "v-net" {
   name                = lower("${var.company}-${var.project}-${var.environment}-vnet")
-  resource_group_name = azurerm_resource_group.main.name
+  resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
-  address_space       = ["10.0.0.0/16"]
-}
-
-# Resource Group for backend
-resource "azurerm_resource_group" "rg-backend" {
-  name     = lower("${var.company}-${var.project}-backend-rg")
-  location = var.location
-}
-
-# Storage Account for backend
-resource "azurerm_storage_account" "sa-backend" {
-  name                     = lower("${var.company}${var.project}backendsa")
-  resource_group_name      = azurerm_resource_group.backend.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  address_space       = ["10.0.0.0/24"]
 }
 
 # Storage Container for Terraform state
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.backend.name
+  storage_account_id    = azurerm_storage_account.storage-acc.id
   container_access_type = "private"
+    }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                  = "nsg"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+
+  security_rule {
+    name                       = "security-rules"
+    priority                   = "100"
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
